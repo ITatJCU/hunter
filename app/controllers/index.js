58d135d97e28ee7cbc57169aa632a1a6ce1a1881
Angus Thomsen <aksthomsen@gmail.com>
2015-04-26 17:21:29 +1000

    setup simple index route + styles

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  needs: ['application'],
+
   count: 0,
+
+  appNameBinding: 'controllers.application.appName',
 
   actions: {
     incrementCount: function () {
