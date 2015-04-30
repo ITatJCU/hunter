883dfb9007e282de799a803af4b2ed35c09cfdfe
Angus Thomsen <aksthomsen@gmail.com>
2015-04-30 13:24:53 +1000

    scroll fade

--- app/views/leader.js
+++ app/views/leader.js
@@ -2,4 +2,20 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
 
+  scrollFadeThreshold: 25,
+
+  setupScrollWatcher: function () {
+    this.$(window).on('scroll', (evt) => {
+      var fadeThreshold = this.get('scrollFadeThreshold');
+
+      if (window.scrollY <= fadeThreshold) {
+        var opacity = (fadeThreshold - window.scrollY) / fadeThreshold;
+        this.$('.page-name').css('opacity', opacity);
+      }
+      else {
+        this.$('.page-name').css('opacity', 0);
+      }
+    });
+  }.on('didInsertElement')
+
 });
