a4eb9dbf5ec40ec58eba7d09614d938ecc6951b5
Angus Thomsen <aksthomsen@gmail.com>
2015-04-30 13:36:48 +1000

    remove scroll event on window change

--- app/views/leader.js
+++ app/views/leader.js
@@ -4,18 +4,43 @@ export default Ember.View.extend({
 
   scrollFadeThreshold: 25,
 
-  setupScrollWatcher: function () {
-    this.$(window).on('scroll', (evt) => {
-      var fadeThreshold = this.get('scrollFadeThreshold');
 
-      if (window.scrollY <= fadeThreshold) {
-        var opacity = (fadeThreshold - window.scrollY) / fadeThreshold;
-        this.$('.page-name').css('opacity', opacity);
-      }
-      else {
-        this.$('.page-name').css('opacity', 0);
-      }
-    });
-  }.on('didInsertElement')
+  setupScrollWatcher: function () {
+    this.$(window).on('scroll', this.onWindowScroll());
+  }.on('didInsertElement'),
+
+
+  tearDownScrollWatcher: function () {
+    this.$(window).off('scroll', this.onWindowScroll());
+  }.on('willDestroyElement'),
+
+
+  //
+  // EVENTS
+  //
+
+  __scrollEvent: null,
+
+  onWindowScroll: function () {
+    var cb = this.get('__scrollEvent');
+
+    if (cb) {
+      return cb;
+    }
+    else {
+      this.set('__scrollEvent', (evt) => {
+        var fadeThreshold = this.get('scrollFadeThreshold');
+
+        if (window.scrollY <= fadeThreshold) {
+          var opacity = (fadeThreshold - window.scrollY) / fadeThreshold;
+          this.$('.page-name').css('opacity', opacity);
+        }
+        else {
+          this.$('.page-name').css('opacity', 0);
+        }
+      });
+      return this.get('__scrollEvent');
+    }
+  }
 
 });
