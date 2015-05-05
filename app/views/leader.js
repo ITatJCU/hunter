896eedb0c491b7d392ddc3646411bee5f4d568b2
Angus Thomsen <aksthomsen@gmail.com>
2015-05-05 14:43:40 +1000

    side bar burger button

--- app/views/leader.js
+++ app/views/leader.js
@@ -6,7 +6,9 @@ export default Ember.View.extend({
 
 
   setupScrollWatcher: function () {
-    this.$(window).on('scroll', this.onWindowScroll());
+    var callback = this.onWindowScroll();
+    callback();
+    this.$(window).on('scroll', callback);
   }.on('didInsertElement'),
 
 
@@ -28,17 +30,24 @@ export default Ember.View.extend({
       return cb;
     }
     else {
+      var fadeSelector = '.nav-bar';
       this.set('__scrollEvent', () => {
         var fadeThreshold = this.get('scrollFadeThreshold');
 
         if (window.scrollY <= fadeThreshold) {
           var opacity = (fadeThreshold - window.scrollY) / fadeThreshold;
-          this.$('.page-name')
-            .css('opacity', opacity)
+          this.$(fadeSelector)
+            .css({
+              opacity: opacity,
+              display: 'initial'
+            })
             .offset({ bottom: window.scrollY/2 });
         }
         else {
-          this.$('.page-name').css('opacity', 0);
+          this.$(fadeSelector).css({
+            opacity: 0,
+            display: 'none'
+          });
         }
       });
       return this.get('__scrollEvent');
