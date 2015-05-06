1ce2129f7a91665d39512c3cf2c32400743d9daa
Angus Thomsen <aksthomsen@gmail.com>
2015-05-06 11:43:51 +1000

    slide in / out side bar

--- app/views/default-layout.js
+++ app/views/default-layout.js
@@ -9,52 +9,70 @@ export default Ember.View.extend({
   scrollFadeThreshold: 25,
 
   setupScrollWatcher: function () {
-    var callback = this.onWindowScroll();
-    callback();
-    this.$(window).on('scroll', callback);
+    var scrollCallback = this.fadeOutTitleBar.bind(this);
+    var sidebarCallback = this.showSideBar.bind(this);
+
+    //
+    // call once for consistency
+    //
+    scrollCallback();
+
+    this.set('__scrollEvent', scrollCallback);
+    this.set('__sidebarEvent', sidebarCallback);
+
+    this.$(window).on('scroll', scrollCallback);
+    this.$('.nav-burger').on('click', sidebarCallback);
   }.on('didInsertElement'),
 
 
   tearDownScrollWatcher: function () {
-    this.$(window).off('scroll', this.onWindowScroll());
+    this.$(window).off('scroll', this.get('__scrollEvent'));
+    this.$('.nav-burger').off('click', this.get('__sidebarEvent'));
   }.on('willDestroyElement'),
 
 
   //
   // EVENTS
   //
+  //   callbacked are saved as private members so they can be
+  //   removed properly and don't hang around after the view
+  //   has been detached
+  //
 
   __scrollEvent: null,
+  __sidebarEvent: null,
 
-  onWindowScroll: function () {
-    var cb = this.get('__scrollEvent');
+  fadeOutTitleBar: function () {
+    var fadeSelector = '.nav-bar';
+    var fadeThreshold = this.get('scrollFadeThreshold');
 
-    if (cb) {
-      return cb;
+    if (window.scrollY <= fadeThreshold) {
+      var opacity = (fadeThreshold - window.scrollY) / fadeThreshold;
+      this.$(fadeSelector)
+        .css({
+          opacity: opacity,
+          display: 'initial'
+        })
+        .offset({ bottom: window.scrollY/2 });
     }
     else {
-      var fadeSelector = '.nav-bar';
-      this.set('__scrollEvent', () => {
-        var fadeThreshold = this.get('scrollFadeThreshold');
-
-        if (window.scrollY <= fadeThreshold) {
-          var opacity = (fadeThreshold - window.scrollY) / fadeThreshold;
-          this.$(fadeSelector)
-            .css({
-              opacity: opacity,
-              display: 'initial'
-            })
-            .offset({ bottom: window.scrollY/2 });
-        }
-        else {
-          this.$(fadeSelector).css({
-            opacity: 0,
-            display: 'none'
-          });
-        }
+      this.$(fadeSelector).css({
+        opacity: 0,
+        display: 'none'
       });
-      return this.get('__scrollEvent');
     }
-  }
+  },
+
+  showSideBar: function () {
+    var self = this;
+    console.log('ping');
+    this.$('.nav-sidebar').removeClass('hidden');
+    Ember.run.later(() => {
+      this.$('.viewport').on('click', function callback() {
+        self.$('.nav-sidebar').addClass('hidden');
+        self.$('.viewport').off('click', callback);
+      });
+    }, 500);
+  },
 
 });
--- app/views/leader.js
+++ app/views/leader.js
@@ -1,4 +1,3 @@
-import Ember from 'ember';
 import DefaultLayoutView from './default-layout';
 
 export default DefaultLayoutView.extend({
