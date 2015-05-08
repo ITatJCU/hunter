f841e1c9fc2f8ba0d353456bc421ad2c79124e85
Angus Thomsen <aksthomsen@gmail.com>
2015-05-08 18:48:57 +1000

    make pretty sign up page

--- app/controllers/register/index.js
+++ app/controllers/register/index.js
@@ -2,15 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+  loading: false,
+  disableSubmit: true,
   playerAlias: '',
 
+  configButtonState: function () {
+    let alias =  this.get('playerAlias');
+    let enoughText = !!alias.length;
+    let disable = !enoughText || this.get('loading');
+    this.set('disableSubmit', disable);
+  }.observes('playerAlias', 'loading'),
+
   actions: {
-    //
-    // TODO wire up properly
-    //
     confirm: function () {
+      if (this.get('disableSubmit')) {
+        return;
+      }
+      this.set('loading', true);
       let alias = this.get('playerAlias');
-      console.log(`ayy lmao you name is ${alias}`);
+      //
+      // TODO create account here
+      //
     }
   }
 
