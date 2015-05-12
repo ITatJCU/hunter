0bca08b97b9be481262d25260bf8d7d65e4bea37
Angus Thomsen <aksthomsen@gmail.com>
2015-05-13 09:34:37 +1000

    creats user

--- app/controllers/register/index.js
+++ app/controllers/register/index.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Controller.extend({
 
@@ -19,10 +20,32 @@ export default Ember.Controller.extend({
         return;
       }
       this.set('loading', true);
-      let alias = this.get('playerAlias');
+
       //
-      // TODO create account here
+      // create a player
       //
+      // would use ember data here but the server api
+      // doesn't really accomodate for this, and fair
+      // enough, due to sercurity related stuff.
+      //
+      let deffered = $.ajax({
+        type: "post",
+        url: '/players',
+        datatype: 'json',
+        data: JSON.stringify({
+          alias: this.get('playerAlias')
+        }),
+      });
+
+      deffered.done((data) => {
+        console.log(data);
+      });
+      deffered.fail((error) => {
+        console.error(error.responseJSON.message);
+      });
+      deffered.always(() => {
+        this.set('loading', false);
+      });
     }
   }
 
