0856d097ec8ab12a48fa1b963ad0af421e9dbda5
Angus Thomsen <aksthomsen@gmail.com>
2015-05-28 15:09:59 +1000

    fixed jshint issue

--- tests/unit/models/code-test.js
+++ tests/unit/models/code-test.js
@@ -2,7 +2,7 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('code', 'Unit | Model | code', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['transform:location', 'mixin:prop-checker'],
 });
 
 test('it exists', function(assert) {
